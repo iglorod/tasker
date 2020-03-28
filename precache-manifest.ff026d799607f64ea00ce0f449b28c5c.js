2f266da2c3651f0cf7dfab3b55522d960473ce66
igor29 <igmatviytsev@gmail.com>
2020-03-28 10:58:26 +0200

    Updates

---
+++ precache-manifest.ff026d799607f64ea00ce0f449b28c5c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08682e7095ea5e456372318a29ebcc08",
+    "url": "/tasker/index.html"
+  },
+  {
+    "revision": "faf367fcea8b78149940",
+    "url": "/tasker/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "10fa9e4f5ab035427f88",
+    "url": "/tasker/static/css/main.672a0ef2.chunk.css"
+  },
+  {
+    "revision": "faf367fcea8b78149940",
+    "url": "/tasker/static/js/2.5421c7da.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/tasker/static/js/2.5421c7da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10fa9e4f5ab035427f88",
+    "url": "/tasker/static/js/main.acf62619.chunk.js"
+  },
+  {
+    "revision": "99efe2fbdaed9a4add19",
+    "url": "/tasker/static/js/runtime-main.36f4f9b4.js"
+  },
+  {
+    "revision": "31eef98b9f04d497fcd461ea8be8fad0",
+    "url": "/tasker/static/media/logo-invert.31eef98b.png"
+  },
+  {
+    "revision": "cdc9dbf8dffb04cef7d1b2df6368cce6",
+    "url": "/tasker/static/media/logo.cdc9dbf8.png"
+  }
+]);
